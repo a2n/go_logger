49252aa287e21ea626385e1d39fccb9a33aa66ac
Alan Lu <alan0098@gmail.com>
2013-01-20 21:47:05 +0800

    First commit.

---
+++ logger.go
@@ -0,0 +1,67 @@
+package logger
+
+import (
+    "log/syslog"
+)
+
+func Info(msg string) {
+    w, err := syslog.Dial("", "", syslog.LOG_INFO, "a2n logger ")
+    defer w.Close()
+    cherr(err)
+    w.Info(msg)
+}
+
+func Debug(msg string) {
+    w, err := syslog.Dial("", "", syslog.LOG_DEBUG, "a2n logger ")
+    defer w.Close()
+    cherr(err)
+    w.Debug(msg)
+}
+
+func Notice(msg string) {
+    w, err := syslog.Dial("", "", syslog.LOG_NOTICE, "a2n logger ")
+    defer w.Close()
+    cherr(err)
+    w.Notice(msg)
+}
+
+func Warning(msg string) {
+    w, err := syslog.Dial("", "", syslog.LOG_WARNING, "a2n logger ")
+    defer w.Close()
+    cherr(err)
+    w.Warning(msg)
+}
+
+func Error(msg string) {
+    w, err := syslog.Dial("", "", syslog.LOG_ERR, "a2n logger ")
+    defer w.Close()
+    cherr(err)
+    w.Err(msg)
+}
+
+func Critical(msg string) {
+    w, err := syslog.Dial("", "", syslog.LOG_CRIT, "a2n logger ")
+    defer w.Close()
+    cherr(err)
+    w.Crit(msg)
+}
+
+func Alert(msg string) {
+    w, err := syslog.Dial("", "", syslog.LOG_ALERT, "a2n logger ")
+    defer w.Close()
+    cherr(err)
+    w.Alert(msg)
+}
+
+func Emerg(msg string) {
+    w, err := syslog.Dial("", "", syslog.LOG_EMERG, "a2n logger ")
+    defer w.Close()
+    cherr(err)
+    w.Emerg(msg)
+}
+
+func cherr(err error) {
+    if err != nil {
+	panic(err)
+    }
+}
---
+++ logger_test.go
@@ -0,0 +1,16 @@
+package logger
+
+import (
+    "testing"
+    "fmt"
+    "crypto/md5"
+    "time"
+    "io"
+)
+
+func TestInfo(t  *testing.T) {
+    h := md5.New()
+    io.WriteString(h, time.Now().String())
+    msg := fmt.Sprintf("%x", h.Sum(nil))
+    Info(msg)
+}
